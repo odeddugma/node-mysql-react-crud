1ebe92e4b824cdd11b85b340128ed776451a2495
Oded Dugma <odeddugma@gmail.com>
2022-02-08 23:15:29 +0200

    Create form

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<div className="workerInfo">
+				<label>Name:</label>
+				<input type="text" />
+				<label>Age:</label>
+				<input type="number" />
+				<label>Country:</label>
+				<input type="text" />
+				<label>Position:</label>
+				<input type="text" />
+				<label>Wage (year):</label>
+				<input type="number" />
+				<button>Add Employee</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
