d10b89b27d127043ac29cd6f839074b3cedc0587
Oded Dugma <odeddugma@gmail.com>
2022-02-08 23:40:08 +0200

    Create states

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+	const [name, setName] = useState("");
+	const [age, setAge] = useState(0);
+	const [country, setCountry] = useState("");
+	const [position, setPosition] = useState("");
+	const [wage, setWage] = useState(0);
+
 	return (
 		<div className="App">
 			<div className="workerInfo">
 				<label>Name:</label>
-				<input type="text" />
+				<input type="text" onChange={(e) => setName(e.target.value)} />
 				<label>Age:</label>
-				<input type="number" />
+				<input type="number" onChange={(e) => setAge(e.target.value)} />
 				<label>Country:</label>
-				<input type="text" />
+				<input type="text" onChange={(e) => setCountry(e.target.value)} />
 				<label>Position:</label>
-				<input type="text" />
+				<input type="text" onChange={(e) => setPosition(e.target.value)} />
 				<label>Wage (year):</label>
-				<input type="number" />
+				<input type="number" onChange={(e) => setWage(e.target.value)} />
 				<button>Add Employee</button>
 			</div>
 		</div>
